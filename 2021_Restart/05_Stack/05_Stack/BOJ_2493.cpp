7b8988cb82b35136e7805cd6ceab3b39402288c4
JINKOO <jacob456@hanmail.net>
2021-02-16 17:06:11 +0900

    2021.02.16 풀이 참조

--- 2021_Restart/05_Stack/05_Stack/BOJ_2493.cpp
+++ 2021_Restart/05_Stack/05_Stack/BOJ_2493.cpp
@@ -4,6 +4,8 @@
 #include <stack>
 using namespace std;
 
+// 시간 초과 코드
+/*
 int find_index(int val, vector<int>& v)
 {
 	for (int i = 0; i < v.size(); i++)
@@ -56,6 +58,47 @@ int main()
 		result.pop();
 	}
 	cout << "\n";
+
+	return 0;
+}
+*/
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N;
+	cin >> N;
+
+	// 0. stack를 선언하는데 pair형의 원소로 한다. pair<heigth, index>
+	stack<pair<int, int>> s;
+
+	for (int i = 0; i < N; i++)
+	{
+		// 1. 수신탑의 높이를 입력받는다.
+		int curr_height;
+		cin >> curr_height;
+
+		// 2. stack이 empty가 아닐 때까지 '현재 들어온 수신탑의 높이'보다 큰 수신탑을 찾는다.
+		while (!s.empty())
+		{
+			// 2.1 curr_height보다 높은 수신탑을 찾으면, 해당 수신탑의 index출력하고 break한다.
+			if (curr_height < s.top().first)
+			{
+				cout << s.top().second << "\n";
+				break;
+			}
+			s.pop();
+		}
+
+		// 3. stack이 empty라면, 수신할 수 있는 수신탑이 없다는 의미이다. 따라서 0출력
+		if (s.empty())
+			cout << 0 << "\n";
+
+		// 4. stack에 push한다.
+		s.push(make_pair(curr_height, i + 1));
+	}
 
 	return 0;
 }
